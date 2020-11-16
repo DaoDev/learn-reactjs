8b2d18f0caee7e1ededc5faac36f8e0cfa4f8d16
DaoDev <boynguyen1998@gmail.com>
2020-11-16 22:05:41 +0700

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dao Nguyen
+          Đạo yêu Linh 
         </p>
         
       </header>
