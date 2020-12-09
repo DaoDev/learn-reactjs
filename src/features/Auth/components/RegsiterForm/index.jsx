e4a038328aaef890ac41a6e477fd7d7f2276888b
dao.nguyen <dao.nguyen@bnksolution.com>
2020-12-09 17:17:18 +0700

    add validation form

--- src/features/Auth/components/RegsiterForm/index.jsx
+++ src/features/Auth/components/RegsiterForm/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import InputField from 'components/form-controls/InputField';
-import { Avatar, Button, makeStyles, Typography } from '@material-ui/core';
-import { LockOutlined } from '@material-ui/icons';
-import PasswordField from 'components/form-controls/PasswordFiled';
+import React from "react";
+import PropTypes from "prop-types";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import InputField from "components/form-controls/InputField";
+import { Avatar, Button, makeStyles, Typography } from "@material-ui/core";
+import { LockOutlined } from "@material-ui/icons";
+import PasswordField from "components/form-controls/PasswordFiled";
 
 const useStyle = makeStyles((theme) => ({
   root: {
@@ -14,12 +14,12 @@ const useStyle = makeStyles((theme) => ({
   },
 
   avatar: {
-    margin: '0 auto',
+    margin: "0 auto",
     backgroundColor: theme.palette.secondary.main,
   },
 
   title: {
-    textAlign: 'center',
+    textAlign: "center",
     margin: theme.spacing(2, 0, 3, 0),
   },
 
@@ -34,14 +34,38 @@ RegisterForm.propTypes = {
 
 function RegisterForm(props) {
   const classes = useStyle();
-  const schema = yup.object().shape({});
+  const schema = yup.object().shape({
+    fullName: yup
+      .string()
+      .required("Please enter your full name.")
+      .test(
+        "should has at least two words", // name
+        "Please enter at least two words.", // measage
+        // dieu kien (ham test)
+        (values) => {
+          return values.split(" ").length >= 2;
+        }
+      ),
+    email: yup
+      .string()
+      .required("Please enter your email")
+      .email("Please enter a valid email address."),
+    password: yup
+      .string()
+      .required("Please enter ypur password")
+      .min(6, "Please enter at least 6 characters"),
+    retypePassword: yup
+      .string()
+      .required("Please retype your password.")
+      .oneOf([yup.ref("password")], "Password does not match"),
+  });
 
   const form = useForm({
     defaultValues: {
-      fullName: '',
-      email: '',
-      password: '',
-      retypePassword: '',
+      fullName: "",
+      email: "",
+      password: "",
+      retypePassword: "",
     },
     resolver: yupResolver(schema),
   });
@@ -69,9 +93,19 @@ function RegisterForm(props) {
         <InputField name="fullName" label="FullName" form={form} />
         <InputField name="email" label="Email" form={form} />
         <PasswordField name="password" label="Password" form={form} />
-        <PasswordField name="retypePassword" label="Retype Password" form={form} />
+        <PasswordField
+          name="retypePassword"
+          label="Retype Password"
+          form={form}
+        />
 
-        <Button type="submit" variant="contained" color="primary" fullWidth className={classes.submit}>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          fullWidth
+          className={classes.submit}
+        >
           Create an account
         </Button>
       </form>
